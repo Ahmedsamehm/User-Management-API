a6b8e8310905e857731950cfe702b75f93a2ca25
Ahmed Sameh Azouz <113038422+Ahmedsamehm@users.noreply.github.com>
2026-03-27 21:45:53 +0200

    Sets up database and user modules
    Removes the default NestJS boilerplate files to establish a more structured application foundation.
    
    Adds core dependencies for database integration with TypeORM and PostgreSQL, along with NestJS configuration, validation, and mapping types.
    
    Integrates a dedicated `DatabaseModule` and `UserModule` into the main application module, preparing for domain-specific features.

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ nullable: false })
+  name: string;
+
+  @Column({ unique: true, nullable: false })
+  email: string;
+
+  @Column({ length: 255, nullable: false })
+  password: string;
+}
